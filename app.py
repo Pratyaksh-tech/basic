1ef69bd24be3c3bef6c4f9043b7e1eb169e5fa3d
Pratyaksh-tech <81953991+Pratyaksh-tech@users.noreply.github.com>
2023-04-30 09:25:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ from flask_socketio import join_room, leave_room, send, SocketIO
 import datetime
 from os import path
 import os
-import eventlet
-eventlet.monkey_patch()
+from gevent import monkey
+monkey.patch_all()
 
 app = Flask(__name__);
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///main.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "etshtssrstjrdtrtsj@dtyjyjrtyj3456rthjrstjrjeyrstyjrsthjr";
-socketio = SocketIO(app, manage_session=False, async_mode='eventlet');
+socketio = SocketIO(app, manage_session=False, async_mode='gevent');
 
 db = SQLAlchemy(app);
 app.app_context().push();
@@ -264,5 +264,5 @@ print("hello")
 
 if __name__ == "__main__":
 	app.run()
-	socketio.run(app, server='eventlet');
+	socketio.run(app);
 
