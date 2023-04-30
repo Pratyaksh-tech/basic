d6ad92bf12db2a6a2feca7be34e86997ad53654b
Pratyaksh-tech <81953991+Pratyaksh-tech@users.noreply.github.com>
2023-04-30 09:35:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__);
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///main.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "etshtssrstjrdtrtsj@dtyjyjrtyj3456rthjrstjrjeyrstyjrsthjr";
-socketio = SocketIO(app, manage_session=False, async_mode='gevent');
+socketio = SocketIO(app, manage_session=False, async_mode='gevent', cors_allowed_origins="*");
 
 db = SQLAlchemy(app);
 app.app_context().push();
